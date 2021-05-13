97b2fcafb22ebd388c1c4ec21d6b43681f5a6f7b
Cianware <leogrises@gmail.com>
2021-05-12 22:18:52 -0400

    agregando footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,20 @@
-/* $(window).scroll(function() {
-    $("#navbar").removeClass( "bg-transparent" ).addClass( "bg-dark" );
-  });
- */
-
-  // Esta función sirve para opacar el backgrond del navbar
+// Esta función sirve para opacar el backgrond del navbar
   var lastScrollTop = 0;
 $(window).scroll(function(event){
    var st = $(this).scrollTop();
    if (st > lastScrollTop){
        // downscroll code
-       $("#navbar").removeClass( "bg-transparent" ).addClass( "bg-dark" );
+       $("#navbar").removeClass( "bg-transparent" ).addClass( "bg-primary" );
    } else {
       // upscroll code
-      $("#navbar").removeClass( "bg-dark" ).addClass( "bg-transparent" );
+      $("#navbar").removeClass( "bg-primary" ).addClass( "bg-transparent" );
    }
    lastScrollTop = st;
 });
 
 
+// Esta función sirve para hacer desaparecer la bienvenida del contacto 
+// y aparecer el formulario de contacto
 $("#btn-comenzar").click(function(){
     $("#pre-formulario").removeClass( "d-flex" ).addClass( "d-none" );
     $("#formulario").removeClass( "d-none" ).addClass( "d-flex" );
